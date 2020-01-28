4f2a41fa02347ff7080bbf6a39852eb95cfe2111
arcum-omni <travis.l.eiler@gmail.com>
2020-01-28 15:49:17 -0800

    Created RawMaterial class with properties

---
+++ RestaurantManagement/RawMaterial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement
+{
+    public class RawMaterial
+    {
+        public string RawMatlName { get; set; }
+
+        public string RawMatlDescription { get; set; }
+
+        public int RawMatlQuantity { get; set; }
+
+        public decimal RawMatlCost { get; set; }
+    }
+}
