5b4b857eb2a28907d6504e27e616c2c1d7d66a3f
arcum-omni <travis.l.eiler@gmail.com>
2020-01-28 16:00:24 -0800

    Created BankAccount class

---
+++ RestaurantManagement/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement
+{
+    public class BankAccount
+    {
+        public string AccountName { get; set; }
+
+        public string AccountDescription { get; set; }
+
+        public decimal AccountBalance { get; set; }
+    }
+}
