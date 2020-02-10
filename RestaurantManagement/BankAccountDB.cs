c66df7b7f3e45b15c09c804c3c4f652dbff96723
arcum-omni <travis.l.eiler@gmail.com>
2020-02-10 07:51:47 -0800

    BankAccountDB Incomplete.

---
+++ RestaurantManagement/BankAccountDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement
+{
+    class BankAccountDB
+    {
+        public static List<BankAccount> GetBankAccounts()
+        {
+            using (var context = new RestaurantContext())
+            {
+                List<BankAccount> bankAccounts = from 
+            }
+        }
+
+        public static List<BankAccount> Add(BankAccount account)
+        {
+            using (var context = new RestaurantContext())
+            {
+                context.BankAccounts.Add(account);
+                context.SaveChanges();
+
+                return account;
+            }
+        }
+    }
+}
