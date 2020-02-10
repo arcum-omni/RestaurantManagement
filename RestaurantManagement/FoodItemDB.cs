b09a7012f5c2d9a259312b02307d35fbaef078d7
EthanMartino <martino9448@student.cptc.edu>
2020-02-09 19:40:53 -0800

    Created FoodItemDB class with basic CRUD functionality

---
+++ RestaurantManagement/FoodItemDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement
+{
+    public static class FoodItemDB
+    {
+        public static List<FoodItem> GetAllFoodItems()
+        {
+            using (var context = new RestaurantContext())
+            {
+                List<FoodItem> foodItems =
+                    context.FoodItems
+                    .OrderBy(item => item.ItemId)
+                    .ToList();
+
+                return foodItems;
+            }
+        }
+
+        public static FoodItem Add(FoodItem item)
+        {
+            using (var context = new RestaurantContext())
+            {
+                context.FoodItems.Add(item);
+                context.SaveChanges();
+
+                return item;
+            }
+        }
+
+        public static void Delete(FoodItem item)
+        {
+            using (var context = new RestaurantContext())
+            {
+                context.FoodItems.Attach(item);
+                context.Entry(item).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public static FoodItem Update(FoodItem item)
+        {
+            using (var context = new RestaurantContext())
+            {
+                context.FoodItems.Attach(item);
+                context.Entry(item).State = EntityState.Modified;
+                context.SaveChanges();
+
+                return item;
+            }
+        }
+    }
+}
