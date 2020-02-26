2d74f00343c602a9ab07dc366f3ab5a2123b0c47
arcum-omni <travis.l.eiler@gmail.com>
2020-02-25 18:54:09 -0800

    Added regions to hide auto generated code to improve readability of our code

--- RestaurantManagement/RestaurantContext.cs
+++ RestaurantManagement/RestaurantContext.cs
@@ -6,22 +6,27 @@ namespace RestaurantManagement
 
     public class RestaurantContext : DbContext
     {
+        #region Auto-Generated Comments
         // Your context has been configured to use a 'RestaurantContext' connection string from your application's 
         // configuration file (App.config or Web.config). By default, this connection string targets the 
         // 'RestaurantManagement.RestaurantContext' database on your LocalDb instance. 
         // 
         // If you wish to target a different database and/or database provider, modify the 'RestaurantContext' 
         // connection string in the application configuration file.
+        #endregion
+
         public RestaurantContext()
             : base("name=RestaurantContext")
         {
         }
 
+        #region Auto-Generated Comments
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
         // public virtual DbSet<MyEntity> MyEntities { get; set; }
-        
+        #endregion
+
         public virtual DbSet<BankAccount> BankAccounts { get; set; }
 
         public virtual DbSet<RawMaterial> RawMaterials { get; set; }
