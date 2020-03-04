89fd8789bb11f5878b486a3469e5ce744c39ad2a
arcum-omni <travis.l.eiler@gmail.com>
2020-03-04 12:59:07 -0800

    Created home screen form, renamed form1 to frmHomeScreen

--- RestaurantManagement/frmHomeScreen.cs
+++ RestaurantManagement/frmHomeScreen.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RestaurantManagement
 {
-    public partial class Form1 : Form
+    public partial class frmHomeScreen : Form
     {
-        public Form1()
+        public frmHomeScreen()
         {
             InitializeComponent();
         }
