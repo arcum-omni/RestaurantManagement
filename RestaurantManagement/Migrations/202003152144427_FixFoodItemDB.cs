00ee0b97be90512318001e136cfece7178cc27b4
EthanMartino <martino9448@student.cptc.edu>
2020-03-15 14:46:18 -0700

    Added new migration

---
+++ RestaurantManagement/Migrations/202003152144427_FixFoodItemDB.cs
@@ -0,0 +1,16 @@
+namespace RestaurantManagement.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixFoodItemDB : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
